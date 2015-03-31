22d99fa328b00d0850f903ea677f95fea6799165
lyc <lyc@lycdeMacBook-Pro.local>
2015-03-31 16:32:37 +0800

    Initial Commit

---
+++ GetLocalPhoto01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetLocalPhoto01
+//
+//  Created by lyc on 15/3/31.
+//  Copyright (c) 2015年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
